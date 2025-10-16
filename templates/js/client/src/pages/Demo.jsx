342c8e09d41a5023cee6492d8a89e020de5e2dff
Prantor-Das <prantor.college@gmail.com>
2025-10-17 00:50:34 +0530

    Update Twitter handle references to reflect the new username

--- templates/js/client/src/pages/Demo.jsx
+++ templates/js/client/src/pages/Demo.jsx
@@ -144,7 +144,7 @@ export default function Demo() {
           </Link>
           &
           <Link
-            to="https://x.com/akashi_sde"
+            to="https://x.com/das_prantor"
             target="_blank"
             className="text-zinc-900 dark:text-zinc-100 hover:text-blue-500 hover:dark:text-blue-400 font-semibold text-lg"
           >
